e92ce35744b8cc420108f4fd4198b4a27468fd40
Uwe Plonus <u.plonus@gmail.com>
2017-12-28 18:17:49 +0100

    Corrected script

--- vars/buildStep.groovy
+++ vars/buildStep.groovy
@@ -13,30 +13,38 @@ def call(body) {
                 globalMavenSettingsConfig: '9a4daf6d-06dd-434a-83cc-9ba9bd2326fc',
                 options: [artifactsPublisher(disabled: true),
                           junitPublisher(disabled: true, ignoreAttachments: false)]) {
-            mvnCmd = "mvn -Dmaven.test.failure.ignore=true clean install"
-            if (${config.doCheckstyle}) {
-                mvnCmd += " checkstyle:checkstyle"
-            }
-            if (${config.doSpotbugs}) {
-                mvnCmd += " findbugs:findbugs"
-            }
-            if (${config.doPmd}) {
-                mvnCmd += " pmd:pmd"
-            }
-            sh mvnCmd
+            sh "mvn -Dmaven.test.failure.ignore=true clean install"
             step([$class: 'Publisher'])
-            if (${config.doCheckstyle}) {
-                checkstyle canComputeNew: false, pattern: '**/checkstyle-result.xml'
-            }
-            if (${config.doSpotbugs}) {
-                findbugs canComputeNew: false, pattern: '**/target/findbugsXml.xml'
-            }
-            if (${config.doJacoco}) {
-                jacoco exclusionPattern: '**/jaxb/*.class'
-            }
-            if (${config.doPmd}) {
-                pmd canComputeNew: false, pattern: '**/pmd.xml'
-            }
+        }
+    }
+    if (${config.doJacoco}) {
+        jacoco exclusionPattern: '**/jaxb/*.class'
+    }
+    if (${config.doCheckstyle}) {
+        withMaven(jdk: 'Current JDK 8',
+                maven: 'Current Maven 3',
+                mavenLocalRepo: '${JENKINS_HOME}/maven-repositories/${EXECUTOR_NUMBER}/',
+                globalMavenSettingsConfig: '9a4daf6d-06dd-434a-83cc-9ba9bd2326fc') {
+            sh "mvn checkstyle:checkstyle"
+            checkstyle canComputeNew: false, pattern: '**/checkstyle-result.xml'
+        }
+    }
+    if (${config.doSpotbugs}) {
+        withMaven(jdk: 'Current JDK 8',
+                maven: 'Current Maven 3',
+                mavenLocalRepo: '${JENKINS_HOME}/maven-repositories/${EXECUTOR_NUMBER}/',
+                globalMavenSettingsConfig: '9a4daf6d-06dd-434a-83cc-9ba9bd2326fc') {
+            sh "mvn findbugs:findbugs"
+            findbugs canComputeNew: false, pattern: '**/target/findbugsXml.xml'
+        }
+    }
+    if (${config.doPmd}) {
+        withMaven(jdk: 'Current JDK 8',
+                maven: 'Current Maven 3',
+                mavenLocalRepo: '${JENKINS_HOME}/maven-repositories/${EXECUTOR_NUMBER}/',
+                globalMavenSettingsConfig: '9a4daf6d-06dd-434a-83cc-9ba9bd2326fc') {
+            sh "mvn pmd:pmd"
+            pmd canComputeNew: false, pattern: '**/pmd.xml'
         }
     }
 }
