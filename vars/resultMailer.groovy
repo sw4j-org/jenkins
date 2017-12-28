3965db92f313f9d79d48fde6d1bb61414339e614
Uwe Plonus <u.plonus@gmail.com>
2017-12-28 15:30:42 +0100

    Created library to contain common steps for pipelines.

---
+++ vars/resultMailer.groovy
@@ -0,0 +1,36 @@
+
+def call() {
+    always {
+        script {
+            if (currentBuild.result == null || currentBuild.result == 'SUCCESS') {
+                if (currentBuild.previousBuild != null && currentBuild.previousBuild.result != 'SUCCESS') {
+                    emailext(
+                            to: 'ci@sw4j.org',
+                            recipientProviders: [[$class: 'CulpritsRecipientProvider'],
+                                                 [$class: 'DevelopersRecipientProvider'],
+                                                 [$class: 'FailingTestSuspectsRecipientProvider'],
+                                                 [$class: 'FirstFailingBuildSuspectsRecipientProvider']],
+                            replyTo: 'ci@sw4j.org',
+                            subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - is successful again',
+                            body: '$PROJECT_NAME - Build # $BUILD_NUMBER - is successful again',
+                            mimeType: 'text/plain')
+                }
+            } else {
+                emailext(
+                        to: 'ci@sw4j.org',
+                        recipientProviders: [[$class: 'CulpritsRecipientProvider'],
+                                             [$class: 'DevelopersRecipientProvider'],
+                                             [$class: 'FailingTestSuspectsRecipientProvider'],
+                                             [$class: 'FirstFailingBuildSuspectsRecipientProvider']],
+                        replyTo: 'ci@sw4j.org',
+                        subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - failed!',
+                        body: '$PROJECT_NAME - Build # $BUILD_NUMBER - failed with state $BUILD_STATUS!\n' +
+                                '\n' +
+                                'Check console output at $BUILD_URL to view the results.',
+                        mimeType: 'text/plain',
+                        attachLog: true,
+                        compressLog: true)
+            }
+        }
+    }
+}
