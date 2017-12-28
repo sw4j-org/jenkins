5f34c41ed44f50eee5eb71e229e3325ffb9fe12f
Uwe Plonus <u.plonus@gmail.com>
2017-12-28 15:37:29 +0100

    Changed library to not contain always

--- vars/resultMailer.groovy
+++ vars/resultMailer.groovy
@@ -1,21 +1,8 @@
 
 def call() {
-    always {
-        script {
-            if (currentBuild.result == null || currentBuild.result == 'SUCCESS') {
-                if (currentBuild.previousBuild != null && currentBuild.previousBuild.result != 'SUCCESS') {
-                    emailext(
-                            to: 'ci@sw4j.org',
-                            recipientProviders: [[$class: 'CulpritsRecipientProvider'],
-                                                 [$class: 'DevelopersRecipientProvider'],
-                                                 [$class: 'FailingTestSuspectsRecipientProvider'],
-                                                 [$class: 'FirstFailingBuildSuspectsRecipientProvider']],
-                            replyTo: 'ci@sw4j.org',
-                            subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - is successful again',
-                            body: '$PROJECT_NAME - Build # $BUILD_NUMBER - is successful again',
-                            mimeType: 'text/plain')
-                }
-            } else {
+    script {
+        if (currentBuild.result == null || currentBuild.result == 'SUCCESS') {
+            if (currentBuild.previousBuild != null && currentBuild.previousBuild.result != 'SUCCESS') {
                 emailext(
                         to: 'ci@sw4j.org',
                         recipientProviders: [[$class: 'CulpritsRecipientProvider'],
@@ -23,14 +10,25 @@ def call() {
                                              [$class: 'FailingTestSuspectsRecipientProvider'],
                                              [$class: 'FirstFailingBuildSuspectsRecipientProvider']],
                         replyTo: 'ci@sw4j.org',
-                        subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - failed!',
-                        body: '$PROJECT_NAME - Build # $BUILD_NUMBER - failed with state $BUILD_STATUS!\n' +
-                                '\n' +
-                                'Check console output at $BUILD_URL to view the results.',
-                        mimeType: 'text/plain',
-                        attachLog: true,
-                        compressLog: true)
+                        subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - is successful again',
+                        body: '$PROJECT_NAME - Build # $BUILD_NUMBER - is successful again',
+                        mimeType: 'text/plain')
             }
+        } else {
+            emailext(
+                    to: 'ci@sw4j.org',
+                    recipientProviders: [[$class: 'CulpritsRecipientProvider'],
+                                         [$class: 'DevelopersRecipientProvider'],
+                                         [$class: 'FailingTestSuspectsRecipientProvider'],
+                                         [$class: 'FirstFailingBuildSuspectsRecipientProvider']],
+                    replyTo: 'ci@sw4j.org',
+                    subject: '$PROJECT_NAME - Build # $BUILD_NUMBER - failed!',
+                    body: '$PROJECT_NAME - Build # $BUILD_NUMBER - failed with state $BUILD_STATUS!\n' +
+                            '\n' +
+                            'Check console output at $BUILD_URL to view the results.',
+                    mimeType: 'text/plain',
+                    attachLog: true,
+                    compressLog: true)
         }
     }
 }
