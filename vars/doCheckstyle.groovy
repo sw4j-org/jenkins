47f8febe68c9ed15165a63f294984c914d8ac793
Uwe Plonus <u.plonus@gmail.com>
2017-12-28 22:46:26 +0100

    Added values for line coverage

--- vars/doCheckstyle.groovy
+++ vars/doCheckstyle.groovy
@@ -1,11 +1,15 @@
 
 def call() {
-
     withMaven(jdk: 'Current JDK 8',
             maven: 'Current Maven 3',
             mavenLocalRepo: '${JENKINS_HOME}/maven-repositories/${EXECUTOR_NUMBER}/',
             globalMavenSettingsConfig: '9a4daf6d-06dd-434a-83cc-9ba9bd2326fc') {
         sh "mvn checkstyle:checkstyle"
-        checkstyle canComputeNew: false, pattern: '**/checkstyle-result.xml'
+        checkstyle canComputeNew: false,
+                pattern: '**/checkstyle-result.xml',
+                failedTotalHigh: '0',
+                unstableTotalHigh: '0',
+                failedTotalNormal: '20',
+                unstableTotalNormal: '10'
     }
 }
