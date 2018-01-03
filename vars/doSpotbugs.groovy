3be73537b32a5ef8098939b01e67cb6cef4a4c57
Uwe Plonus <u.plonus@gmail.com>
2018-01-03 14:38:10 +0100

    Changed findbugs call to spotbugs

--- vars/doSpotbugs.groovy
+++ vars/doSpotbugs.groovy
@@ -5,7 +5,7 @@ def call() {
             maven: 'Current Maven 3',
             mavenLocalRepo: '${JENKINS_HOME}/maven-repositories/${EXECUTOR_NUMBER}/',
             globalMavenSettingsConfig: '9a4daf6d-06dd-434a-83cc-9ba9bd2326fc') {
-        sh "mvn findbugs:findbugs"
+        sh "mvn spotbugs:spotbugs"
         findbugs canComputeNew: false, pattern: '**/target/findbugsXml.xml'
     }
 }
