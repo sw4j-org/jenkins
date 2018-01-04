196443188e260d583ecf8872e7c21ed5371065d7
Uwe Plonus <u.plonus@gmail.com>
2018-01-04 22:12:50 +0100

    Added thresholds to pmd and spotbugs

--- vars/doPmd.groovy
+++ vars/doPmd.groovy
@@ -6,6 +6,9 @@ def call() {
             mavenLocalRepo: '${JENKINS_HOME}/maven-repositories/${EXECUTOR_NUMBER}/',
             globalMavenSettingsConfig: '9a4daf6d-06dd-434a-83cc-9ba9bd2326fc') {
         sh "mvn pmd:pmd"
-        pmd canComputeNew: false, pattern: '**/pmd.xml'
+        pmd canComputeNew: false,
+                pattern: '**/pmd.xml',
+                failedTotalHigh: '0',
+                unstableTotalNormal: '0'
     }
 }
--- vars/doSpotbugs.groovy
+++ vars/doSpotbugs.groovy
@@ -6,6 +6,9 @@ def call() {
             mavenLocalRepo: '${JENKINS_HOME}/maven-repositories/${EXECUTOR_NUMBER}/',
             globalMavenSettingsConfig: '9a4daf6d-06dd-434a-83cc-9ba9bd2326fc') {
         sh "mvn spotbugs:spotbugs"
-        findbugs canComputeNew: false, pattern: '**/target/spotbugsXml.xml'
+        findbugs canComputeNew: false,
+                pattern: '**/target/spotbugsXml.xml',
+                failedTotalHigh: '0',
+                unstableTotalNormal: '0'
     }
 }
